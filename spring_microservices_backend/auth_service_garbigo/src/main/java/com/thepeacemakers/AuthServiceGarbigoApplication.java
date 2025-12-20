5108f6c7fb655840c45576284814faa5aa8f2896
Bill Graham Peacemaker <grahambill011@gmail.com>
2025-12-20 17:08:14 +0300

    Add backend microservices project

---
+++ spring_microservices_backend/auth_service_garbigo/src/main/java/com/thepeacemakers/AuthServiceGarbigoApplication.java
@@ -0,0 +1,13 @@
+package com.thepeacemakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceGarbigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceGarbigoApplication.class, args);
+	}
+
+}
