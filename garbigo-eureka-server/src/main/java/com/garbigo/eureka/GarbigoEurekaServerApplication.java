97542493367febf945d1316940bc0776f349d2d6
Bill Graham Peacemaker <grahambill011@gmail.com>
2026-01-06 12:03:13 +0300

    Created the Eureka server

---
+++ garbigo-eureka-server/src/main/java/com/garbigo/eureka/GarbigoEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.garbigo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbigoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbigoEurekaServerApplication.class, args);
+	}
+
+}
---
+++ garbigo-eureka-server/src/test/java/com/garbigo/eureka/GarbigoEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garbigo.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbigoEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
