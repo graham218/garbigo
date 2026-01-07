7fdf017a868414afa040d397b0c84ea777ce0915
Bill Graham Peacemaker <grahambill011@gmail.com>
2026-01-07 12:44:34 +0300

    Debugging auth service

--- garbigo_frontend/lib/core/config/app_config.dart
+++ garbigo_frontend/lib/core/config/app_config.dart
@@ -1,7 +1,5 @@
 class AppConfig {
   static const String baseUrl = 'http://localhost:8080';
-  // For physical device or web: use your machine IP
-  // static const String baseUrl = 'http://192.168.1.100:8080';
   static const String authBase = '$baseUrl/auth';
   static const String usersBase = '$baseUrl/users';
 }
